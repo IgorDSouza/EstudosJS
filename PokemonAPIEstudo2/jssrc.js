d13d89d62468c415c0c93fbf256c4b1c96032310
IgorD.Souza <igor251999@hotmail.com>
2022-08-12 00:20:59 -0300

    'AttPokedex'

--- PokemonAPIEstudo2/jssrc.js
+++ PokemonAPIEstudo2/jssrc.js
@@ -1,12 +1,17 @@
 
 const pickPokemon= () =>{
-    const id = document.querySelector('[name="fname"').value
+    const id = document.querySelector('[name="fname"]').value
 
-     fetchPokemon(id)
+    if( id == '' ){
 
+        window.alert("Nome ou ID inválido")
+
+    } else{
+       fetchPokemon(id) 
+    }
 
 }
-    
+
 
 const fetchPokemon= (id) => {
 
@@ -29,20 +34,35 @@ const fetchPokemon= (id) => {
     
             const lisPokemons = pokemons.reduce((accumulator,pokemon)=>{// o reduce foi usado para transformar todas as arrays devolvidas pela api em uma unica string
                 const types = pokemon.types.map(typeInfo => typeInfo.type.name) //o map foi usado para selecionar o tipo do pokemon que estava dentro de outra array e  retorna-lo em string
-    
-    
+                const abilities = pokemon.abilities.map(abilityInfo => abilityInfo.ability.name)
+
+                console.log(pokemon)
                 accumulator+= `
                 <li id="${pokemon.name}" >
-                <img class=" ${types[0]}" alt="${pokemon.name}" src=" https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png" 
-                <h1>${pokemon.id} . ${pokemon.name}</h1>
-                <p> ${types.join(' | ')}</p>     
+
+                <h3></br>${pokemon.id}.${pokemon.name}</h3>
+                <p> Type: </br>
+                ${types.join(' | ')}</p>   
+                <p> Abilities: </br>
+                ${abilities.join(' | ')} </p>
                 </li>`
+                return accumulator // o acumulator verifica cada pokemon que foi passado pelo parametro, trata como o html acima e concatena, tornando uma grande string de pokemons
+                
+            },'') 
+
+            const imgPokemons = pokemons.reduce((accumulator,pokemon)=>{// o reduce foi usado para transformar todas as arrays devolvidas pela api em uma unica string
+                const types = pokemon.types.map(typeInfo => typeInfo.type.name) //o map foi usado para selecionar o tipo do pokemon que estava dentro de outra array e  retorna-lo em string
+
+                accumulator+= `
+                <img class=" ${types[0]}" alt="${pokemon.name}" src=" https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png"> `
                 return accumulator // o acumulator verifica cada pokemon que foi passado pelo parametro, trata como o html acima e concatena, tornando uma grande string de pokemons
                 
             },'') 
             console.log(lisPokemons)
             const ol = document.querySelector('[data-js="pokedex"]')
+            const img = document.querySelector('[data-js="pokeimg"]')
 
+            img.innerHTML =imgPokemons
             ol.innerHTML =lisPokemons   
         })
     
